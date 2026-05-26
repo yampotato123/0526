57ef6c4d6cdd2ac818e83d50a5df674e8b109ac8
yampotato123 <tonya22406@gmail.com>
2026-05-26 12:30:07 +0800

    加入專案檔案。

---
+++ ContentChange/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ContentChange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入一個90以上的數字整數");
+
+            if (int.TryParse(Console.ReadLine(), out int max))
+            {
+                if (max <= 90)
+                {
+                    Console.WriteLine("請輸入大於90的數字整數");
+                }
+
+                for (int i = 1; i <= max; i++)
+                {
+                    if (i % 3 == 0 && i % 5 == 0)
+                    {
+                        Console.WriteLine("Dann");
+                    }
+                    else if (i % 3 == 0)
+                    {
+                        Console.WriteLine("Build");
+                    }
+                    else if (i % 5 == 0)
+                    {
+                        Console.WriteLine("School");
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ itsnotKFC/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace itsnotKFC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            label1 = new Label();
+            button12 = new Button();
+            button13 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(281, 59);
+            button1.Name = "button1";
+            button1.Size = new Size(50, 50);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(281, 124);
+            button2.Name = "button2";
+            button2.Size = new Size(50, 50);
+            button2.TabIndex = 0;
+            button2.Text = "button1";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(337, 59);
+            button3.Name = "button3";
+            button3.Size = new Size(50, 50);
+            button3.TabIndex = 0;
+            button3.Text = "button1";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(337, 124);
+            button4.Name = "button4";
+            button4.Size = new Size(50, 50);
+            button4.TabIndex = 0;
+            button4.Text = "button1";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(393, 59);
+            button5.Name = "button5";
+            button5.Size = new Size(50, 50);
+            button5.TabIndex = 0;
+            button5.Text = "button1";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(393, 124);
+            button6.Name = "button6";
+            button6.Size = new Size(50, 50);
+            button6.TabIndex = 0;
+            button6.Text = "button1";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(281, 184);
+            button7.Name = "button7";
+            button7.Size = new Size(50, 50);
+            button7.TabIndex = 0;
+            button7.Text = "button1";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(281, 249);
+            button8.Name = "button8";
+            button8.Size = new Size(50, 50);
+            button8.TabIndex = 0;
+            button8.Text = "button1";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(337, 184);
+            button9.Name = "button9";
+            button9.Size = new Size(50, 50);
+            button9.TabIndex = 0;
+            button9.Text = "button1";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(337, 249);
+            button10.Name = "button10";
+            button10.Size = new Size(50, 50);
+            button10.TabIndex = 0;
+            button10.Text = "button1";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(393, 184);
+            button11.Name = "button11";
+            button11.Size = new Size(50, 50);
+            button11.TabIndex = 0;
+            button11.Text = "button1";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(524, 276);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 23);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // button12
+            // 
+            button12.Location = new Point(492, 184);
+            button12.Name = "button12";
+            button12.Size = new Size(93, 50);
+            button12.TabIndex = 2;
+            button12.Text = "button12";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(653, 184);
+            button13.Name = "button13";
+            button13.Size = new Size(93, 50);
+            button13.TabIndex = 2;
+            button13.Text = "button12";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(29, 107);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(147, 27);
+            radioButton1.TabIndex = 3;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(29, 184);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(147, 27);
+            radioButton2.TabIndex = 4;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(label1);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button10);
+            Controls.Add(button11);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Label label1;
+        private Button button12;
+        private Button button13;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+    }
+}
