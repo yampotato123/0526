398e4548de147266dcfa91b6141715d18bac3561
yampotato123 <tonya22406@gmail.com>
2026-05-26 15:49:05 +0800

    StringGroup

---
+++ StringGroupBy/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StringGroupBy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                        .ToLower()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .GroupBy(w => w)
+                        .OrderByDescending(g => g.Count());
+            foreach (var a in input)
+            {
+                Console.WriteLine($"{a.Key}: {a.Count()}");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
