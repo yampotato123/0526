f3111046a667fa17850749dde87735cdcedd5042
yampotato123 <tonya22406@gmail.com>
2026-06-01 06:40:14 +0800

    guessnumber

--- GuessNumber/Form1.Designer.cs
+++ GuessNumber/Form1.Designer.cs
@@ -28,81 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            listBox1 = new ListBox();
-            button1 = new Button();
-            button2 = new Button();
-            textBox1 = new TextBox();
-            button3 = new Button();
-            button4 = new Button();
+            listHistory = new ListBox();
+            btnStart = new Button();
+            btnAns = new Button();
+            txtGuess = new TextBox();
+            btnGiveUp = new Button();
+            btnEnter = new Button();
             SuspendLayout();
             // 
-            // listBox1
+            // listHistory
             // 
-            listBox1.FormattingEnabled = true;
-            listBox1.Location = new Point(387, 32);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(326, 372);
-            listBox1.TabIndex = 0;
+            listHistory.FormattingEnabled = true;
+            listHistory.Location = new Point(387, 32);
+            listHistory.Name = "listHistory";
+            listHistory.Size = new Size(326, 372);
+            listHistory.TabIndex = 0;
             // 
-            // button1
+            // btnStart
             // 
-            button1.Font = new Font("黑体", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(43, 32);
-            button1.Name = "button1";
-            button1.Size = new Size(107, 46);
-            button1.TabIndex = 1;
-            button1.Text = "Start";
-            button1.UseVisualStyleBackColor = true;
+            btnStart.Font = new Font("黑体", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnStart.Location = new Point(43, 32);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(107, 46);
+            btnStart.TabIndex = 1;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnAns
             // 
-            button2.Font = new Font("黑体", 10F, FontStyle.Bold);
-            button2.Location = new Point(43, 129);
-            button2.Name = "button2";
-            button2.Size = new Size(107, 46);
-            button2.TabIndex = 1;
-            button2.Text = "LOOK ANS";
-            button2.UseVisualStyleBackColor = true;
+            btnAns.Font = new Font("黑体", 10F, FontStyle.Bold);
+            btnAns.Location = new Point(43, 129);
+            btnAns.Name = "btnAns";
+            btnAns.Size = new Size(107, 46);
+            btnAns.TabIndex = 1;
+            btnAns.Text = "LOOK ANS";
+            btnAns.UseVisualStyleBackColor = true;
+            btnAns.Click += btnAns_Click;
             // 
-            // textBox1
+            // txtGuess
             // 
-            textBox1.Location = new Point(43, 264);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(259, 30);
-            textBox1.TabIndex = 2;
+            txtGuess.Location = new Point(43, 264);
+            txtGuess.Name = "txtGuess";
+            txtGuess.Size = new Size(259, 30);
+            txtGuess.TabIndex = 2;
             // 
-            // button3
+            // btnGiveUp
             // 
-            button3.Font = new Font("黑体", 10F, FontStyle.Bold);
-            button3.Location = new Point(43, 346);
-            button3.Name = "button3";
-            button3.Size = new Size(107, 46);
-            button3.TabIndex = 1;
-            button3.Text = "GiveUp";
-            button3.UseVisualStyleBackColor = true;
+            btnGiveUp.Font = new Font("黑体", 10F, FontStyle.Bold);
+            btnGiveUp.Location = new Point(43, 346);
+            btnGiveUp.Name = "btnGiveUp";
+            btnGiveUp.Size = new Size(107, 46);
+            btnGiveUp.TabIndex = 1;
+            btnGiveUp.Text = "GiveUp";
+            btnGiveUp.UseVisualStyleBackColor = true;
+            btnGiveUp.Click += btnGiveUp_Click;
             // 
-            // button4
+            // btnEnter
             // 
-            button4.Font = new Font("黑体", 10F, FontStyle.Bold);
-            button4.Location = new Point(209, 346);
-            button4.Name = "button4";
-            button4.Size = new Size(107, 46);
-            button4.TabIndex = 1;
-            button4.Text = "Enter";
-            button4.UseVisualStyleBackColor = true;
-            button4.Click += btnEnter_Click;
+            btnEnter.Font = new Font("黑体", 10F, FontStyle.Bold);
+            btnEnter.Location = new Point(209, 346);
+            btnEnter.Name = "btnEnter";
+            btnEnter.Size = new Size(107, 46);
+            btnEnter.TabIndex = 1;
+            btnEnter.Text = "Enter";
+            btnEnter.UseVisualStyleBackColor = true;
+            btnEnter.Click += btnEnter_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(11F, 23F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(textBox1);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(listBox1);
+            Controls.Add(txtGuess);
+            Controls.Add(btnEnter);
+            Controls.Add(btnGiveUp);
+            Controls.Add(btnAns);
+            Controls.Add(btnStart);
+            Controls.Add(listHistory);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -111,11 +113,11 @@
 
         #endregion
 
-        private ListBox listBox1;
-        private Button button1;
-        private Button button2;
-        private TextBox textBox1;
-        private Button button3;
-        private Button button4;
+        private ListBox listHistory;
+        private Button btnStart;
+        private Button btnAns;
+        private TextBox txtGuess;
+        private Button btnGiveUp;
+        private Button btnEnter;
     }
 }
--- GuessNumber/Form1.cs
+++ GuessNumber/Form1.cs
@@ -24,11 +24,63 @@ namespace GuessNumber
 
             answer = string.Join("", numbers);
 
+            txtGuess.Clear();
+            listHistory.Items.Clear();
 
+            MessageBox.Show("NEW GAME!!!");
         }
         private void btnEnter_Click(object sender, EventArgs e)
         {
-            string input = 
+            string input = txtGuess.Text.Trim(); //trim去除空白
+
+            if (input.Length != 4 || !input.All(char.IsDigit))
+            {
+                MessageBox.Show("輸入無效");
+                return;
+            }
+            if (input.Distinct().Count() != 4)
+            {
+                MessageBox.Show("請輸入4個不重複數字");
+                return;
+            }
+
+            int countA = 0;
+            int countB = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (input[i] == answer[i])
+                {
+                    countA++;
+                }
+
+                else if (answer.Contains(input[i])) //前面已經過濾 "位置相同"
+                {
+                    countB++;
+                }
+            }
+
+            string resultText = $"{input} => {countA} A {countB}B";
+            listHistory.Items.Add(resultText);
+
+            if (countA == 4)
+            {
+                MessageBox.Show($"通關!");
+                Restartgame();
+            }
+
+            
+        }
+
+        private void btnGiveUp_Click(object sender, EventArgs e)
+        {
+            Restartgame();
+        }
+
+        private void btnAns_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"答案是{answer}");
+            Restartgame();
         }
     }
 }
